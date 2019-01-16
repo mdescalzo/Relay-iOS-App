240c4a77434e07fb3c6fe3914c95b4a877903a9b
Mark Descalzo <mdescalzo@mac.com>
2019-01-16 11:10:50 -0700

    Fix for improper text appearing in search results.

--- RelayServiceKit/src/Protocols/ContactsManagerProtocol.h
+++ RelayServiceKit/src/Protocols/ContactsManagerProtocol.h
@@ -4,15 +4,14 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-//@class CNContact;
-//@class Contact;
-//@class PhoneNumber;
 @class RelayRecipient;
 @class UIImage;
+@class YapDatabaseReadTransaction;
 
 @protocol ContactsManagerProtocol <NSObject>
 
 - (nullable NSString *)displayNameForRecipientId:(NSString *)recipientId;
+- (nullable NSString *)displayNameForRecipientId:(NSString *)recipientId transaction:(YapDatabaseReadTransaction *)transaction;
 - (nullable NSString *)cachedDisplayNameForRecipientId:(NSString *)recipientId;
 
 - (NSArray<RelayRecipient *> *)allRecipients;
@@ -24,6 +23,7 @@ NS_ASSUME_NONNULL_BEGIN
                          withRecipient:(RelayRecipient *)right NS_SWIFT_NAME(compare(recipient:with:));
 
 -(nullable RelayRecipient *)recipientWithId:(NSString *)recipientId;
+-(nullable RelayRecipient *)recipientWithId:(NSString *)recipientId transaction:(YapDatabaseReadTransaction *)transaction;
 //- (nullable CNContact *)cnContactWithId:(nullable NSString *)contactId;
 //- (nullable NSData *)avatarDataForCNContactId:(nullable NSString *)contactId;
 - (nullable UIImage *)avatarImageRecipientId:(NSString *)recipientId;
