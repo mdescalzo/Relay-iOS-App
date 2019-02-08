3d28bfdaf9aa0b65cf9d405328e8d5a0f9dd22be
Mark Descalzo <mdescalzo@mac.com>
2019-02-08 14:16:15 -0700

    Bringing in new call view controller

---
+++ Relay/src/ViewControllers/Calls/PeerViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PeerViewCell.swift
+//  CallUITestBed
+//
+//  Created by Mark Descalzo on 2/4/19.
+//  Copyright © 2019 Ringneck Software, LLC. All rights reserved.
+//
+
+import UIKit
+
+class PeerViewCell: UICollectionViewCell {
+
+   
+    @IBOutlet weak var avView: UIView!
+    @IBOutlet weak var avatarImageView: UIImageView!
+    
+}
